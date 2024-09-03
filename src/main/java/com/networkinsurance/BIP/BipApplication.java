9ba08b00d4e8b8e3ddb67ef57c4f6f9848290fc2
Adrekx <ayush9931.mishra@gmail.com>
2024-09-03 23:21:41 +0530

    Initial Commit

---
+++ src/main/java/com/networkinsurance/BIP/BipApplication.java
@@ -0,0 +1,13 @@
+package com.networkinsurance.BIP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/networkinsurance/BIP/BipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.networkinsurance.BIP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
